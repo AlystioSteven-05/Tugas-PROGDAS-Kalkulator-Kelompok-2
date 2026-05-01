d24e387a1a08b6bf9cc783ffc966b54d400f3fb6
ZFrees <jaysen.mi0719@gmail.com>
2026-05-01 10:09:25 +0700

    Fungsi Ganjil-Genap Belum Ada

--- kalkulator_kelompok2.c
+++ kalkulator_kelompok2.c
@@ -30,60 +30,71 @@ float perkalian (float input1, float input2){
 }
 
 float pembagian (float input1, float input2){
-    if (input2 <= 0){
+    if (input2 == 0){
         return -1;
     }
     float hasil = input1 / input2;
     return hasil;
 }
 
-float pangkat (float bil_pangkat, float input1){
-    return pow(input1, bil_pangkat);
+// float ganjil_genap (){}
+
+float pangkat (float input1, float bil_pangkat){
+    float hasil = pow(input1, bil_pangkat);
+    return hasil;
 }
 
 int main (){
     int pilihan;
     float input1, input2, result;
 
-    printf("Pilih:  ");
-    scanf("%d", &pilihan);
+    do{
+        displayheader();
+        printf("Pilih:  ");
+        scanf("%d", &pilihan);
 
-    switch(pilihan){
+        switch(pilihan){
         case 1:
         printf("Masukkan kedua angka: ");
-        scanf("%d %d", &input1, &input2);
+        scanf("%f %f", &input1, &input2);
         result = penjumlahan(input1, input2);
-        printf("Hasil:  %f", result);
+        printf("Hasil:  %.2f\n", result);
         break;
         case 2:
         printf("Masukkan kedua angka: ");
-        scanf("%d %d", &input1, &input2);
+        scanf("%f %f", &input1, &input2);
         result = pengurangan(input1, input2);
-        printf("Hasil:  %f", result);
+        printf("Hasil:  %.2f\n", result);
         break;
         case 3:
         printf("Masukkan kedua angka: ");
-        scanf("%d %d", &input1, &input2);
+        scanf("%f %f", &input1, &input2);
         result = perkalian(input1, input2);
-        printf("Hasil:  %f", result);
+        printf("Hasil:  %.2f\n", result);
         break;
         case 4:
         printf("Masukkan kedua angka: ");
-        scanf("%d %d", &input1, &input2);
+        scanf("%f %f", &input1, &input2);
         result = pembagian(input1, input2);
-        printf("Hasil:  %f", result);
+        printf("Hasil:  %.2f\n", result);
         break;
         case 5:
-        printf("Masukkan kedua angka: ");
-        scanf("%d %d", &input1, &input2);
-        result = pangkat(input1, input2);
-        printf("Hasil:  %f", result);
+        // Bikin fungsi ganjil_genap
         break;
         case 6:
+        printf("Masukkan kedua angka: ");
+        scanf("%f %f", &input1, &input2);
+        result = pangkat(input1, input2);
+        printf("Hasil:  %.2f\n", result);
+        break;
+        case 0:
         printf("Terima Kasih\n");
         break;
         default:
         printf("Input Invalid\n");
-    }
+        break;
+        }
+    }while(pilihan != 0);
+
     return 0;
 }
